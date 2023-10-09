f2961193dc1ff69ae28eca478981c239d1a74120
linhapeter <linha.peter.jr@gmail.com>
2023-10-09 13:20:29 +0200

    Complexity reduced to 0(n)

--- src/tasks/secondLargestNum.js
+++ src/tasks/secondLargestNum.js
@@ -2,7 +2,15 @@ export const secondLargestNum = (arr) => {
   if (arr.length < 2) {
     throw new Error("Array needs at least 2 elements");
   }
-  const arrWithoutLargest = arr.filter((number) => number != Math.max(...arr));
-  const result = Math.max(...arrWithoutLargest);
-  return result;
+
+  let first = Number.NEGATIVE_INFINITY;
+  let second = Number.NEGATIVE_INFINITY;
+
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] > first) {
+      second = first;
+      first = arr[index];
+    } else if (arr[index] > second) second = arr[index];
+  }
+  return second;
 };
